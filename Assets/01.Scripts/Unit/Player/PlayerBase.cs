3dda0f554e71b18ee0c94c7f73f22c4fc3736084
DESKTOP-0BTASLT\khjtoy <khjgogle@gmail.com>
2023-01-03 23:09:15 +0900

    2023.01.03 PlyaerStats

--- Assets/01.Scripts/Unit/Player/PlayerBase.cs
+++ Assets/01.Scripts/Unit/Player/PlayerBase.cs
@@ -10,10 +10,12 @@ public class PlayerBase : UnitBase
     
     [SerializeField] private PlayerMove playerMove = null;
     [SerializeField] private PlayerAttack playerAttack = null;
+    [SerializeField] private PlayerStats playerStats = null;
     protected override void Init()
     {
         playerMove = AddBehaviour<PlayerMove>(playerMove);
         playerAttack = AddBehaviour<PlayerAttack>(playerAttack);
+        playerStats = AddBehaviour<PlayerStats>(playerStats);
     }
 
     protected override void Awake()
