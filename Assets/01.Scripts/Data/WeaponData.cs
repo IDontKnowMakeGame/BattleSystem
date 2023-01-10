1de610fbe05314d77b31c5a9d433ec536e71ac48
KHJ_COM2 <ggm.s21002@ggh.goe.go.kr>
2023-01-10 23:29:51 +0900

    2023.01.10 exceltoclass Bug

--- Assets/01.Scripts/Data/WeaponData.cs
+++ Assets/01.Scripts/Data/WeaponData.cs
@@ -2,14 +2,16 @@ public class LongSwordData
 {
     public int skilltime = 15;
 	public int hello = 5;
+	
 }
 public class GreatSwordData
 {
     public float ghd = 5.0f;
 	public int gh = 1;
+	
 }
 public class OneSwordData
 {
     public int check = 0;
-	public int er = 2;
+	
 }
