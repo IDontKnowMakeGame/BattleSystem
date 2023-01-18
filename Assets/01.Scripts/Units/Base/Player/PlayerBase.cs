ec1bc1eead7c769a961fb4fcb70979a9c5b4b95b
KHJ_COM2 <ggm.s21002@ggh.goe.go.kr>
2023-01-18 13:36:31 +0900

    2023.01.18 Delete New InputSystem

--- Assets/01.Scripts/Units/Base/Player/PlayerBase.cs
+++ Assets/01.Scripts/Units/Base/Player/PlayerBase.cs
@@ -8,39 +8,11 @@ namespace Units.Base.Player
     {
         [SerializeField] private PlayerMove playerMove; 
         [SerializeField] private PlayerAttack PlayerAttack; 
-        public PlayerInput Input { get; private set; }
         protected override void Init()
         {
-            Input = GetComponent<PlayerInput>();
-
             AddBehaviour(thisStat);
             playerMove = AddBehaviour<PlayerMove>();
             PlayerAttack = AddBehaviour<PlayerAttack>();
-
-            AddInputActionsCallbacks();
         }
-
-        #region Input
-        private void AddInputActionsCallbacks()
-        {
-            Input.PlayerActions.Move.performed += playerMove.Translate;
-        }
-
-        private void RemoveInputActionsCallbacks()
-        {
-            Input.PlayerActions.Move.started -= playerMove.Translate;
-        }
-
-        public void OnApplicationQuit()
-        {
-            RemoveInputActionsCallbacks();
-        }
-
-        protected override void OnDestroy()
-        {
-            base.OnDestroy();
-            RemoveInputActionsCallbacks();
-        }
-        #endregion
     }
 }
