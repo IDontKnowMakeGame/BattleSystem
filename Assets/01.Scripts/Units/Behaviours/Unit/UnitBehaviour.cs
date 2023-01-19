9f9f728c33447a6893d73a4c6740457d43c1210c
KDH <jetmax68@gmail.com>
2023-01-19 13:41:24 +0900

    병합

--- Assets/01.Scripts/Units/Behaviours/Unit/UnitBehaviour.cs
+++ Assets/01.Scripts/Units/Behaviours/Unit/UnitBehaviour.cs
@@ -1,10 +1,13 @@
 ﻿using Units.Base.Unit;
 using Units.Behaviours.Base;
+using System;
 
 namespace Units.Behaviours.Unit
 {
     public class UnitBehaviour : Behaviour
     {
         public new UnitBase ThisBase => base.ThisBase as UnitBase;
+
+        public Action onBehaviourEnd;
     }
 }
--- Assets/01.Scripts/Units/Core/Item/Equi/Weapon/StraightSword/OldStraightSword.cs
+++ Assets/01.Scripts/Units/Core/Item/Equi/Weapon/StraightSword/OldStraightSword.cs
@@ -1,61 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Units.Behaviours.Unit;
 public class OldStraightSword : BaseStraightSword
 {
-	//public override void Start()
-	//{
-	//	base.Start();
-	//	GetWeaponStateData("sword");
-	//	_maxTime = LongSwordData.coolTime;
-	//}
-	//protected override void Skill()
-	//{
-	//	if (_isCoolTime)
-	//		return;
+	public override void Start()
+	{
+		base.Start();
+		//GetWeaponStateData("sword");
+		//_maxTime = LongSwordData.coolTime;
+	}
+	protected override void Skill()
+	{
+		if (_isCoolTime)
+			return;
 
-	//	if (isSkill)
-	//		return;
+		if (isSkill)
+			return;
 
-	//	if (_inputManager.GetKeyInput(InputManager.InputSignal.Skill))
-	//	{
-	//		if (_inputManager.GetKeyDownInput(InputManager.InputSignal.MoveForward))
-	//		{
-	//			RollSkill(Vector3.forward * 2);
-	//		}
-	//		if (_inputManager.GetKeyDownInput(InputManager.InputSignal.MoveBackward))
-	//		{
-	//			RollSkill(Vector3.back * 2);
-	//		}
-	//		if (_inputManager.GetKeyDownInput(InputManager.InputSignal.MoveLeft))
-	//		{
-	//			RollSkill(Vector3.left * 2);
-	//		}
-	//		if (_inputManager.GetKeyDownInput(InputManager.InputSignal.MoveRight))
-	//		{
-	//			RollSkill(Vector3.right * 2);
-	//		}
-	//	}
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			if (Input.GetKeyDown(KeyCode.UpArrow))
+			{
+				RollSkill(Vector3.forward * 2);
+			}
+			if (Input.GetKeyDown(KeyCode.DownArrow))
+			{
+				RollSkill(Vector3.back * 2);
+			}
+			if (Input.GetKeyDown(KeyCode.LeftArrow))
+			{
+				RollSkill(Vector3.left * 2);
+			}
+			if (Input.GetKeyDown(KeyCode.RightArrow))
+			{
+				RollSkill(Vector3.right * 2);
+			}
+		}
 
-	//	if (_inputManager.GetKeyUpInput(InputManager.InputSignal.Skill) && !_isCoolTime)
-	//	{
-	//		_isCoolTime = true;
-	//		RollSkill(Vector3.forward * 2);
-	//	}
-	//}
+		if (Input.GetKeyDown(KeyCode.Space) && !_isCoolTime)
+		{
+			_isCoolTime = true;
+			RollSkill(Vector3.forward * 2);
+		}
+	}
 
-	//private void RollSkill(Vector3 vec)
-	//{
-	//	isSkill = true;
-	//	_isCoolTime = true;
-	//	UnitMove playerMove = _baseObject.GetBehaviour<UnitMove>();
-	//	playerMove.onBehaviourEnd = RollSkillEnd;
-	//	playerMove.Translate(vec * 2, LongSwordData.skillTime);
-	//}
+	private void RollSkill(Vector3 vec)
+	{
+		isSkill = true;
+		_isCoolTime = true;
+		UnitMove unitMove = _thisBase.GetBehaviour<UnitMove>();
+		unitMove.onBehaviourEnd = RollSkillEnd;
+		unitMove.Translate(vec * 2 /*, speed*/);
+	}
 
-	//private void RollSkillEnd()
-	//{
-	//	isSkill = false;
-	//}
+	private void RollSkillEnd()
+	{
+		isSkill = false;
+	}
 }
