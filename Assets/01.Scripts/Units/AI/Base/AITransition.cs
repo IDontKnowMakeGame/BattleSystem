893c42ccdd10091572c5d14c69de2c1854a33f1a
csh4430 <snghun6889@gmail.com>
2023-01-21 21:24:54 +0900

    Unit FSM

---
+++ Assets/01.Scripts/Units/AI/Base/AITransition.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Unit.Base.AI
+{
+    public class AITransition
+    {
+        private AIState targetState;
+        private List<AICondition> conditions = new();
+        
+        private bool isAllConditions = false;
+
+        public void SetLogicCondition(bool logic)
+        {
+            isAllConditions = logic;
+        }
+        
+        public void SetTarget(AIState target)
+        {
+            targetState = target;
+        }
+        
+        public void AddCondition(AICondition condition)
+        {
+            conditions.Add(condition);
+        }
+
+        public bool CheckCondition()
+        {
+            var result = isAllConditions;
+
+            foreach (var condition in conditions)
+            {
+                if (isAllConditions)
+                {
+                    result &= condition.CheckCondition();
+                }
+                else
+                {
+                    result |= condition.CheckCondition();
+                }
+            }
+            
+            return result;
+        }
+        
+        public AIState NextState()
+        {
+            return targetState;
+        }
+    }
+}
---
+++ Assets/01.Scripts/Units/Behaviours/Unit/UnitFSM.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unit.Base.AI;
+using UnityEditor.Rendering;
+using UnityEngine;
+using Behaviour = Units.Behaviours.Base.Behaviour;
+
+public class UnitFSM : Behaviour
+{
+    private AIState currentState;
+    private AIState nextState = null;
+    private readonly Dictionary<Type, AIState> states = new();
+    
+    
+    public bool ChangeState(Type state)
+    {
+        if (states.ContainsKey(state) == false)
+        {
+            Debug.LogError("State " + state + " does not exist");
+            return false;
+        }
+        currentState = states[state];
+        nextState = null;
+        return true;
+    }
+
+    private AIState AddState(Type state, AIState aiState)
+    {
+        if (states.ContainsKey(state) == false)
+        {
+            aiState.unit = ThisBase;
+            aiState.Awake();
+            states.Add(state, aiState);
+        }
+
+        return aiState;
+    }
+
+    public void SetDefaultState<T>() where T : AIState, new()
+    {
+        var roamingState = new T();
+        currentState = AddState(typeof(T), roamingState);
+    }
+
+    public override void Update()
+    {
+        nextState = currentState.ExcuteState();
+        if (nextState != null)
+        {
+            if (states.ContainsKey(nextState.GetType()) == false)
+                AddState(nextState.GetType(), nextState);
+            ChangeState(nextState.GetType());
+        }
+    }
+}
