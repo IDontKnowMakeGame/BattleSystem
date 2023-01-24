f89eb6987d48c3f3733bea6f4582f91487d6753d
KHJ_COM2 <ggm.s21002@ggh.goe.go.kr>
2023-01-24 13:43:47 +0900

    2023.01.24 Astar

--- Assets/01.Scripts/Managements/Managers/MapManager.cs
+++ Assets/01.Scripts/Managements/Managers/MapManager.cs
@@ -17,5 +17,26 @@ namespace Managements.Managers
         {
             return _map[pos];
         }
+
+        public List<BlockBase> GetNeighbors(BlockBase tile)
+        {
+            List<BlockBase> neighbors = new List<BlockBase>();
+            int[,] temp = { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
+
+            for (int i = 0; i < 4; i++)
+            {
+                int checkX = tile.X + temp[i, 0];
+                int checkZ = tile.Z + temp[i, 1];
+
+                Vector3 checkPos = new Vector3(checkX, 0, checkZ);
+
+                if (_map.ContainsKey(checkPos))
+                {
+                    neighbors.Add(_map[checkPos]);
+                }
+            }
+
+            return neighbors;
+        }
     }
 }
