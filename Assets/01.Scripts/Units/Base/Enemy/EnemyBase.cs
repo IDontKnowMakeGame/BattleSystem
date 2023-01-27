47128ecae7adeba39cb08f0647619770d69a358d
csh4430 <snghun6889@gmail.com>
2023-01-27 17:02:58 +0900

    enemy stat

--- Assets/01.Scripts/Units/Base/Enemy/EnemyBase.cs
+++ Assets/01.Scripts/Units/Base/Enemy/EnemyBase.cs
@@ -1,9 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using Units.Base.Unit;
+using Units.Behaviours.Unit;
 using UnityEngine;
 
 public class EnemyBase : UnitBase
 {
-    
+    [SerializeField] protected UnitStat thisStat;
 }
