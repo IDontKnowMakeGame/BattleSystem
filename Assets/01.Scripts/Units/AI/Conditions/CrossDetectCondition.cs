5c5f430fc28d6e8aea399d7f652ae600e25396ee
csh4430 <snghun6889@gmail.com>
2023-01-28 21:07:54 +0900

    Back Attack Created

--- Assets/01.Scripts/Units/AI/Conditions/CrossDetectCondition.cs
+++ Assets/01.Scripts/Units/AI/Conditions/CrossDetectCondition.cs
@@ -1,28 +1,22 @@
 ﻿using System.Collections.Generic;
 using Core;
 using Unit.Base.AI;
+using Units.Base.Unit;
 using UnityEngine;
 
 namespace Unit.Enemy.AI.Conditions
 {
-    public class CrossDetectCondition : AICondition
+    public class CrossDetectCondition : DetectCondition
     {
         private List<Vector3> _crossPoints = new () { Vector3.forward , Vector3.back, Vector3.left, Vector3.right};
-        private int _crossDistance = 1;
-        
         protected override bool CheckConditionInternal()
         {
             foreach (var crossPoint in _crossPoints)
             {
-                if(InGame.BossBase.Position + crossPoint * _crossDistance == InGame.PlayerBase.Position)
+                if(_owner.Position + crossPoint * _distance == _target.Position)
                     return true;
             }
             return false;
-        }
-
-        public void SetDistance(int value)
-        {
-            _crossDistance = value;
         }
     }
 }
---
+++ Assets/01.Scripts/Units/AI/Conditions/DetectCondition.cs
@@ -0,0 +1,22 @@
+﻿using Unit.Base.AI;
+using Units.Base.Unit;
+
+namespace Unit.Enemy.AI.Conditions
+{
+    public class DetectCondition : AICondition
+    {
+        protected UnitBase _target, _owner;
+        protected int _distance = 1;
+
+        public void SetUnits(UnitBase target, UnitBase owner)
+        {
+            _target = target;
+            _owner = owner;
+        }
+        
+        public void SetDistance(int value)
+        {
+            _distance = value;
+        }
+    }
+}
