69de3c6bca51b2f3bb3f06aba33411ad0311e5f9
csh4430 <snghun6889@gmail.com>
2023-01-31 00:27:35 +0900

    area check

---
+++ Assets/01.Scripts/Units/AI/Conditions/AreaCheckCondition.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Unit.Enemy.AI.Conditions
+{
+    public class AreaCheckCondition : DetectCondition
+    {
+        private Vector3 _startPosition, _endPosition;
+        
+        protected override bool CheckConditionInternal()
+        {
+            if (_startPosition.x >= _target.Position.x && _target.Position.x >= _endPosition.x)
+            {
+                Debug.Log("First");
+                if (_startPosition.z >= _target.Position.z && _target.Position.z >= _endPosition.z)
+                {
+                    Debug.Log("Second");
+                    return true;
+                }
+
+            }
+            return false;
+        }
+        
+        public void SetArea(Vector3 startPosition, Vector3 endPosition)
+        {
+            _startPosition = startPosition;
+            _endPosition = endPosition;
+        }
+    }
+}
