60982cb6195f5581b8858c68bc7284e19f05f04a
KDH <jetmax68@gmail.com>
2023-02-27 19:54:55 +0900

    brokenSword

--- Assets/01.Scripts/Units/Core/Item/Equi/Weapon/StraightSword/BrokenSword.cs
+++ Assets/01.Scripts/Units/Core/Item/Equi/Weapon/StraightSword/BrokenSword.cs
@@ -5,13 +5,42 @@ using UnityEngine;
 
 public class BrokenSword : BaseStraightSword
 {
+	private int count = 0;
+
+	public override void Awake()
+	{
+		base.Awake();
+		GetWeaponStateData("brokenSword");
+	}
 	public override void ChangeKey()
 	{
 		base.ChangeKey();
 		InputManager.OnSkillPress -= Skill;
+		_playerAttack.onBehaviourEnd = CountUp;
+	}
+
+	private void CountUp()
+	{
+		count++;
+		if(count == 99)
+		{
+			Skill();
+		}
+		else if(count == 100)
+		{
+			count = 0;
+			GetWeaponStateData("brokenSword");
+		}
 	}
 	protected override void Skill()
 	{
+		_weaponStats.Atk = 900000;
+	}
 
+	public override void Reset()
+	{
+		base.Reset();
+		count = 0;
+		_playerAttack.onBehaviourEnd = null;
 	}
 }
