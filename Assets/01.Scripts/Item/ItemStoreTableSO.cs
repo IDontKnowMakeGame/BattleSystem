428e70964f4b09e91f6df9f35cecba6fe9cadcd1
ehddbs <ehddbs295@gmail.com>
2023-03-04 22:19:58 +0900

    Complate StoreUI

---
+++ Assets/01.Scripts/Item/ItemStoreTableSO.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ItemPrice
+{
+    public int itemID;
+    public int price;
+}
+
+[CreateAssetMenu(menuName ="SO/ItemStore/ItemTable")]
+public class ItemStoreTableSO : ScriptableObject
+{
+    public List<ItemPrice> table;
+}
