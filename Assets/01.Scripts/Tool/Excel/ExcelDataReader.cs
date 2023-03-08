10d00d1d558ef29b02498fc96ff0c53049611194
csh4430 <snghun6889@gmail.com>
2023-03-08 22:20:07 +0900

    Spreadsheet Data Manager

---
+++ Assets/01.Scripts/Tool/Excel/ExcelDataReader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+using UnityEngine.Networking;
+
+namespace Tool.Excel
+{
+    public static class ExcelDataReader
+    {
+        public static string ID { get; set; }
+
+        public static string URL { get; set; }
+        
+        public static string StartIdx = "";
+        public static string EndIdx = "";
+        public static bool IsParsing = false;
+        public static bool IsProcessing = false;
+        public static string Data;
+
+        public static async void GetData(Action<string> callback)
+        {
+            string url;
+            url = $"https://docs.google.com/spreadsheets/d/{ID}/export?format=tsv&range={StartIdx}:{EndIdx}";
+            var www = UnityWebRequest.Get(url);
+            await www.SendWebRequest();
+            Data = www.downloadHandler.text;
+            callback?.Invoke(Data);
+        }
+    }
+}
---
+++ Assets/01.Scripts/Tool/Map/WeaponDataLoader.cs
@@ -0,0 +1,39 @@
+﻿using Tool.Data.Json;
+using Tool.Data.Json.ParsingList;
+using Tool.Excel;
+using UnityEditor;
+using UnityEngine;
+
+namespace Tool.Weapon
+{
+#if UNITY_EDITOR
+    public class WeaponDataLoader : EditorWindow
+    {
+        [MenuItem("Tools/WeaponDataLoader")]
+        public static void ShowWindow()
+        {
+            GetWindow<WeaponDataLoader>("WeaponDataLoader");
+        }
+
+        private async void OnGUI()
+        {
+            GUILayout.Label("WeaponDataLoader", EditorStyles.boldLabel);
+
+            GUILayout.Label("URL Setting", EditorStyles.miniBoldLabel);
+            ExcelDataReader.ID = EditorGUILayout.TextField("ID", ExcelDataReader.ID);
+            ExcelDataReader.StartIdx = EditorGUILayout.TextField("Json Cell", ExcelDataReader.StartIdx);
+
+            if (GUILayout.Button("Generation"))
+            {
+                ExcelDataReader.EndIdx = ExcelDataReader.StartIdx;
+                JsonManager.ReadSheetToJson<WeaponLists>(ExcelDataReader.ID);
+            }
+        }
+
+        private void SaveData(string data)
+        {
+            JsonManager.ReadSheetToJson<WeaponLists>(data);
+        }
+    }
+#endif
+}
