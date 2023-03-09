686998008448a663c74cc167747d820ba5f8f745
KDH <jetmax68@gmail.com>
2023-03-09 12:19:59 +0900

    itemManger

--- Assets/01.Scripts/Item/EquipmentItem/Weapon/StraightSword/BaseStriaghtSword.cs
+++ Assets/01.Scripts/Item/EquipmentItem/Weapon/StraightSword/BaseStriaghtSword.cs
@@ -2,17 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BaseStriaghtSword : MonoBehaviour
+public class BaseStriaghtSword : Weapon
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public override void UseItem()
+	{
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	}
 }
---
+++ Assets/01.Scripts/Item/ItemManager.cs
@@ -0,0 +1,13 @@
+using Managements.Managers;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemManager : Manager
+{
+	//Dictionary<>
+	public override void Start()
+	{
+
+	}
+}
