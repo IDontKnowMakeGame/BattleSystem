5a951f2c55b9f9452f3157b435d98ebaeae14e07
KimDongYun <ehddbs295@gmail.com>
2023-03-16 20:07:51 +0900

    Merge Bug Fix

--- Assets/01.Scripts/Core/Pool/Poolable.cs
+++ Assets/01.Scripts/Core/Pool/Poolable.cs
@@ -1,14 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-<<<<<<<< HEAD:Assets/01.Scripts/Core/Pool/Poolable.cs
 public class Poolable : MonoBehaviour
 {
     public bool isUsing = false;
-========
-public class Bow : Weapon
-{
-
->>>>>>>> DevtheDive:Assets/01.Scripts/Item/EquiqmentItem/Weapon/Bow.cs
 }
