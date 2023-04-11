65e0171d82af678bc2dbd801562368e50bb0022e
KHJ_COM2 <ggm.s21002@ggh.goe.go.kr>
2023-04-11 09:59:18 +0900

    2023.04.11 Halo 완성

---
+++ Assets/01.Scripts/Item/EquiqmentItem/Halo/HaloOfPollution.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Actors.Characters;
+using Core;
+
+public class HaloOfPollution : Halo
+{
+    private int damage;
+
+    public override void Equiqment(CharacterActor actor)
+    {
+        percent = 5;
+        damage = 50;
+        Define.GetManager<EventManager>().StartListening(EventFlag.PollutionHalo, Using);
+    }
+
+    public override void UnEquipment(CharacterActor actor)
+    {
+        Define.GetManager<EventManager>()?.StopListening(EventFlag.PollutionHalo, Using);
+    }
+
+
+    public override void Update()
+    {
+
+    }
+
+    protected override void Using(EventParam eventParam)
+    {
+        if (ConditionCheck())
+        {
+            Debug.Log(eventParam.unit);
+            eventParam.unit.GetAct<CharacterStatAct>().Damage(damage, InGame.Player);
+        }
+    }
+}
