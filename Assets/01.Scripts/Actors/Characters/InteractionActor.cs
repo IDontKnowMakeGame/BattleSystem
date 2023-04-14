e63c67a50fd562f6ab00a82783ee8abef4a75820
KHJ_COM2 <ggm.s21002@ggh.goe.go.kr>
2023-04-14 15:10:54 +0900

    2023.04.14 TorchInteration

---
+++ Assets/01.Scripts/Actors/Characters/InteractionActor.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Core;
+using Managements.Managers;
+using Actors.Characters;
+
+public class InteractionActor : CharacterActor
+{
+    protected override void Init()
+    {
+        base.Init();
+
+        InputManager<Weapon>.OnInteractionPress += Interact;
+    }
+
+    public virtual void Interact()
+    {
+        if (InGame.Player.Position.IsNeighbor(Position) == false) return;
+
+
+        //TODO : 상호작용
+    }
+}
