9ad1d616a8e9ae022f4da94447dfa3b0e11dcaf7
kdh123456 <jetmax68@gmail.com>
2023-04-17 00:50:10 +0900

    scene loader & broken Boxs Add

--- Assets/01.Scripts/Actors/Characters/CharacterActor.cs
+++ Assets/01.Scripts/Actors/Characters/CharacterActor.cs
@@ -8,61 +8,62 @@ using UnityEngine;
 
 namespace Actors.Characters
 {
-    [Flags]
-    public enum CharacterState
-    {
-        None = 0,
-        Move = 1 << 0,
-        Attack = 1 << 1,
-        Skill = 1 << 2,
-        Hold = 1 << 3,
-        StopMove = 1 << 4,              
-        Stun = 1 << 5,
-        Equip = 1 << 6,
-        NuckBack = 1<<7, 
-        Everything = ~None,
-    }
-    public class CharacterActor : Actor
-    {
-        [SerializeField] private CharacterRender _characterRender;
-        [SerializeField] private CharacterState _characterState;
-
-        public Weapon currentWeapon;
+	[Flags]
+	public enum CharacterState
+	{
+		None = 0,
+		Move = 1 << 0,
+		Attack = 1 << 1,
+		Skill = 1 << 2,
+		Hold = 1 << 3,
+		StopMove = 1 << 4,
+		Stun = 1 << 5,
+		Equip = 1 << 6,
+		NuckBack = 1 << 7,
+		Everything = ~None,
+	}
+	public class CharacterActor : Actor
+	{
+		[SerializeField] private CharacterRender _characterRender;
+		[SerializeField] private CharacterState _characterState;
+		[SerializeField] private bool _isBlocking = false;
+		public Weapon currentWeapon;
 		protected override void Init()
-        {
-            base.Init();
-            AddAct(_characterRender);
-        }
+		{
+			base.Init();
+			AddAct(_characterRender);
+		}
 
-        protected override void Awake()
-        {
-            base.Awake();
-        }
+		protected override void Awake()
+		{
+			base.Awake();
+		}
 
-        protected override void Start()
-        {
-            InGame.SetActorOnBlock(this, Position);
-            base.Start();
-        }
-        
-        public void AddState(CharacterState state)
-        {
-            _characterState |= state;
-        }
-        
-        public void RemoveState(CharacterState state)
-        {
-            _characterState &= ~state;
-        }
-        
-        public bool HasState(CharacterState state)
-        {
-            return (_characterState & state) != CharacterState.None;
-        }
+		protected override void Start()
+		{
+			if (_isBlocking)
+				InGame.SetActorOnBlock(this, Position);
+			base.Start();
+		}
 
-        public bool HasAnyState()
-        {
-            return (_characterState & CharacterState.Everything) != CharacterState.None;
-        }    
-    }
+		public void AddState(CharacterState state)
+		{
+			_characterState |= state;
+		}
+
+		public void RemoveState(CharacterState state)
+		{
+			_characterState &= ~state;
+		}
+
+		public bool HasState(CharacterState state)
+		{
+			return (_characterState & state) != CharacterState.None;
+		}
+
+		public bool HasAnyState()
+		{
+			return (_characterState & CharacterState.Everything) != CharacterState.None;
+		}
+	}
 }
