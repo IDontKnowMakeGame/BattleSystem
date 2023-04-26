830c9b2f62ed1e58713407938c0c568194210ef4
KimDongYun <ehddbs295@gmail.com>
2023-04-26 22:44:33 +0900

    MapSystem

--- Assets/01.Scripts/UI/UIFirstFloorMap.cs
+++ Assets/01.Scripts/UI/UIFirstFloorMap.cs
@@ -1,3 +1,4 @@
+using Core;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Search;
@@ -8,13 +9,17 @@ public class UIFirstFloorMap : UIBase
 {
     private const Floor floor = Floor.First;
 
-    private VisualElement mapCast;
+    private VisualElement _mapCast;
+
+    private Dictionary<int,bool> cristalDictionary = new Dictionary<int,bool>();
+
 
     public override void Init()
     {
         _root = UIManager.Instance._document.rootVisualElement.Q<VisualElement>("UI_FirstFloorMap");
 
-        mapCast = _root.Q<VisualElement>("Cast");
+        _mapCast = _root.Q<VisualElement>("Cast");
+        CristalInit();
     }
 
     public void FlagDisplay(bool flag)
@@ -33,9 +38,31 @@ public class UIFirstFloorMap : UIBase
         FlagDisplay(true);
         MapCast();
     }
+    public void CristalInit()
+    {
+        for(int i = 0;i<4;i++)
+        {
+            cristalDictionary.Add(i, false);
+        }
+    }
 
     public void MapCast()
     {
+        List<int> list = Define.GetManager<DataManager>().OnCristalData(Floor.First);
 
+        foreach (int i in list)
+        {
+                cristalDictionary[i] = true;
+        }
+
+        CastOnMap();
+    }
+
+    public void CastOnMap()
+    {
+        if(cristalDictionary[0] && cristalDictionary[1])
+        {
+            _mapCast.Q<VisualElement>("1").style.display = DisplayStyle.None;
+        }
     }
 }
