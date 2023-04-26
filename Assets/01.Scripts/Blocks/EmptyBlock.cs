4554998a317e3999054d6517877c1296ccfe45fc
csh4430 <snghun6889@gmail.com>
2023-04-26 19:30:32 +0900

    낙사블럭

---
+++ Assets/01.Scripts/Blocks/EmptyBlock.cs
@@ -0,0 +1,16 @@
+﻿namespace Blocks
+{
+    public class EmptyBlock : Block
+    {
+        protected override void Update()
+        {
+            base.Update();
+            if(ActorOnBlock == null) return;
+            if (ActorOnBlock.Position == Position)
+            {
+                var stat = ActorOnBlock.GetAct<CharacterStatAct>();
+                stat?.Damage(int.MaxValue, this);
+            }
+        }
+    }
+}
