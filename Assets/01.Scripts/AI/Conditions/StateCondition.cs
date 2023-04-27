49f8b34077b7ae9427fadf4e555f8d11edcc93d3
csh4430 <snghun6889@gmail.com>
2023-04-28 00:12:14 +0900

    prefab

---
+++ Assets/01.Scripts/AI/Conditions/StateCondition.cs
@@ -0,0 +1,18 @@
+﻿using Actors.Characters;
+
+namespace AI.Conditions
+{
+    public class StateCondition : AiCondition
+    {
+        public CharacterState State;
+        
+        public override bool IsSatisfied()
+        {
+            var character = _thisActor as CharacterActor;
+            if (character == null)
+                return false;
+            var result = character.HasState(State);
+            return result;
+        }
+    }
+}
