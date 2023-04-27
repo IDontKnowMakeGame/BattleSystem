d34a44c7e69d0604bb77fe7cb67ca922ec8d01a4
kdh123456 <jetmax68@gmail.com>
2023-04-27 20:30:46 +0900

    old bow shade

---
+++ Assets/ChestObject.cs
@@ -0,0 +1,32 @@
+using Core;
+using Data;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class ChestObject : InteractionActor
+{
+	private bool isOpen = false;
+
+	[SerializeField]
+	private GetItemObject _itemObject;
+
+	protected override void Start()
+	{
+		_itemObject.canInteraction = false;
+	}
+
+	public override void Interact()
+	{
+		if (isOpen)
+			return;
+
+		isOpen = true;
+	}
+
+	public void ChestEnd()
+	{
+		_itemObject.canInteraction = true;
+	}
+}
---
+++ Assets/GetItemObject.cs
@@ -0,0 +1,34 @@
+using Core;
+using Data;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetItemObject : InteractionActor
+{
+	[SerializeField]
+	private ItemID _id;
+
+	[SerializeField]
+	private int _count;
+
+	[SerializeField]
+	private bool _isWeapon;
+
+	public bool canInteraction = true;
+
+	public void Init(ItemID id, int count, bool weapon)
+	{
+		_id = id;
+		_count = count;
+		_isWeapon = weapon;
+	}
+
+	public override void Interact()
+	{
+		if (!canInteraction)
+			return;
+		//여기다가 추가
+		Define.GetManager<ResourceManager>().Destroy(this.gameObject);
+	}
+}
