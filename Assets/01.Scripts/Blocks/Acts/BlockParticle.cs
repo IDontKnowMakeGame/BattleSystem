49439855d26fa751adb17e0de24c42b3ce57e1ad
csh4430 <snghun6889@gmail.com>
2023-04-28 14:40:24 +0900

    파티클 풀링

--- Assets/01.Scripts/Blocks/Acts/BlockParticle.cs
+++ Assets/01.Scripts/Blocks/Acts/BlockParticle.cs
@@ -1,34 +1,49 @@
-﻿using Acts.Base;
+﻿using System.Collections;
+using Acts.Base;
+using Core;
 using UnityEngine;
 
 namespace Blocks.Acts
 {
     public class BlockParticle : Act
     {
-        private ParticleSystem _explosionParticleSystem;
-        private ParticleSystem _smokeParticleSystem;
+        private GameObject _explosionParticleObject;
+        private GameObject _smokeParticleObject;
+        PoolManager _poolManager;
         public override void Awake()
         {
+            _explosionParticleObject = Resources.Load<GameObject>("Prefabs/BlockExplosion");
+            _smokeParticleObject = Resources.Load<GameObject>("Prefabs/BlockSmoke");
+            _poolManager = Define.GetManager<PoolManager>();
             base.Awake();
-            _explosionParticleSystem = CreateParticle("BlockExplosion");
-            _smokeParticleSystem = CreateParticle("BlockSmoke");
         }
         
         public void PlayExplosionParticle()
         {
-            _explosionParticleSystem.Play();
+            ThisActor.StartCoroutine(PlayParticleCoroutine(_explosionParticleObject));
         }
 
         public void PlaySmokeParticle()
         {
-            _smokeParticleSystem.Play();
+            ThisActor.StartCoroutine(PlayParticleCoroutine(_smokeParticleObject));
         }
         
-        public ParticleSystem CreateParticle(string name)
+        public IEnumerator PlayParticleCoroutine(GameObject obj)
         {
-            var particleTrm = Resources.Load<Transform>("Prefabs/" + name);
-            var particle = GameObject.Instantiate(particleTrm, ThisActor.transform);
-            return particle.GetComponent<ParticleSystem>();
+            var particleSystem = CreateParticle(obj, out var poolable);
+            particleSystem.Play();
+            yield return new WaitForSeconds(particleSystem.main.duration);
+            particleSystem.Stop();
+            _poolManager.Push(poolable);
+        }
+        
+        public ParticleSystem CreateParticle(GameObject obj, out Poolable poolable)
+        {
+             poolable =_poolManager.Pop(obj);
+            var particleSystem = poolable.GetComponent<ParticleSystem>();
+            particleSystem.transform.position = ThisActor.Position + Vector3.up * 0.5f;
+            particleSystem.transform.rotation = Quaternion.identity;
+            return particleSystem;
         }
     }
 }
