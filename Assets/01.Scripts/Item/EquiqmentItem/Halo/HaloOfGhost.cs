9c0b696e065a8f4cfe9dc853223f8071e884d2d2
KHJ_COM2 <ggm.s21002@ggh.goe.go.kr>
2023-05-25 10:40:03 +0900

    2023.05.25 Halo 수정

--- Assets/01.Scripts/Item/EquiqmentItem/Halo/HaloOfGhost.cs
+++ Assets/01.Scripts/Item/EquiqmentItem/Halo/HaloOfGhost.cs
@@ -6,9 +6,21 @@ using Core;
 
 public class HaloOfGhost : Halo
 {
+    private bool check = false;
     public override void Equiqment(CharacterActor actor)
     {
-        InGame.Player.GetAct<PlayerStatAct>().Plus(StatType.MAXHP, 100);
+        PlayerStatAct _playerStatAct = InGame.Player.GetAct<PlayerStatAct>();
+        if (_playerStatAct.ChangeStat.hp >= _playerStatAct.ChangeStat.maxHP)
+            check = true;
+        else
+            check = false;
+
+
+        _playerStatAct.Plus(StatType.MAXHP, 100);
+        if(check)
+        {
+            _playerStatAct.Heal(100);
+        }
         UIManager.Instance.InGame.ChanageMaxHP((int)InGame.Player.GetAct<PlayerStatAct>().ChangeStat.maxHP / 10);
         UIManager.Instance.InGame.ChangeCurrentHP(InGame.Player.GetAct<PlayerStatAct>().PercentHP());
     }
@@ -16,7 +28,13 @@ public class HaloOfGhost : Halo
     public override void UnEquipment(CharacterActor actor)
     {
         Debug.Log("UnEquipement");
-        InGame.Player.GetAct<PlayerStatAct>().Sub(StatType.MAXHP, 100);
+        PlayerStatAct _playerStatAct = InGame.Player.GetAct<PlayerStatAct>();
+        _playerStatAct.Sub(StatType.MAXHP, 100);
+        if(check)
+        {
+            _playerStatAct.Heal(0);
+        }
+
         UIManager.Instance.InGame.ChanageMaxHP((int)InGame.Player.GetAct<PlayerStatAct>().ChangeStat.maxHP / 10);
         UIManager.Instance.InGame.ChangeCurrentHP(InGame.Player.GetAct<PlayerStatAct>().PercentHP());
     }
