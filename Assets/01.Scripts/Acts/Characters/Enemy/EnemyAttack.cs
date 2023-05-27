8468547e93079c410125e75fda014c9e6902f052
김정훈 <77821635+kjh075250@users.noreply.github.com>
2023-05-27 14:06:34 +0900

    sound Manager Modify

--- Assets/01.Scripts/Acts/Characters/Enemy/EnemyAttack.cs
+++ Assets/01.Scripts/Acts/Characters/Enemy/EnemyAttack.cs
@@ -30,7 +30,7 @@ namespace Acts.Characters.Enemy
             //Define.GetManager<MapManager>().AttackBlock(ThisActor.Position + dir, DefaultStat.Atk, DefaultStat.Ats, CharacterActor, MovementType.None, isLast);
             if(ThisActor is BossActor)
             {
-                Define.GetManager<SoundManager>().PlayAtPoint("Boss/explosion", ThisActor.Position + dir, 1);
+                //Define.GetManager<SoundManager>().PlayAtPoint("Boss/explosion", ThisActor.Position + dir, 1);
             }
             InGame.Attack(ThisActor.Position + dir, new Vector3(1, 0, 1), DefaultStat.Atk, DefaultStat.Ats, CharacterActor, isLast);
         }
