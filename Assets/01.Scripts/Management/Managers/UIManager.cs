3cc040285da65a446c8ffc54e950061649bc1015
KimDongYun <67079959+ehddbs3408@users.noreply.github.com>
2023-05-31 19:47:34 +0900

    FontAsset

--- Assets/01.Scripts/Management/Managers/UIManager.cs
+++ Assets/01.Scripts/Management/Managers/UIManager.cs
@@ -36,9 +36,9 @@ public class UIManager : MonoBehaviour
 
     private void Start()
     {
-        Define.GetManager<DataManager>().AddItemInInventory(ItemID.CompassOfSpace);
-        Define.GetManager<DataManager>().AddItemInInventory(ItemID.HaloOfTime);
-        Define.GetManager<DataManager>().AddItemInInventory(ItemID.ExecutionBlade);
+        //Define.GetManager<DataManager>().AddItemInInventory(ItemID.CompassOfSpace);
+        //Define.GetManager<DataManager>().AddItemInInventory(ItemID.HaloOfTime);
+        //Define.GetManager<DataManager>().AddItemInInventory(ItemID.ExecutionBlade);
         //Define.GetManager<DataManager>().AddItemInInventory(ItemID.SecondMap);
         //Define.GetManager<DataManager>().AddItemInInventory(ItemID.Pick,1);
         UIStart();
