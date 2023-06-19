657e9ebd433cffff2934fa161e014ed77bc4d9e6
csh4430 <snghun6889@gmail.com>
2023-06-20 00:11:32 +0900

    add particle to empty block

--- Assets/01.Scripts/Blocks/EmptyBlock.cs
+++ Assets/01.Scripts/Blocks/EmptyBlock.cs
@@ -1,4 +1,5 @@
 ﻿using Actors.Characters;
+using Core;
 using UnityEngine;
 
 namespace Blocks
@@ -6,20 +7,14 @@ namespace Blocks
     public class EmptyBlock : Block
     {
         protected bool isParent = false;
+        private Transform anchorTrm;
         protected override void Start()
         {
             base.Start();
-            var anchorTrm = transform.Find("Anchor");
-            var lightObj = new GameObject("Light");
-            var light = lightObj.AddComponent<Light>();
-            light.type = LightType.Point;
-            light.range = 1.5f;
-            light.intensity = 5f;
-            light.color = Color.white;
-            light.shadows = LightShadows.Hard;
-            light.renderMode = LightRenderMode.ForceVertex;
-            lightObj.transform.SetParent(anchorTrm);
-            lightObj.transform.localPosition = Vector3.up * -0.2f;
+            anchorTrm = transform.Find("Anchor");
+            CreateLight();
+            CreateParticle();
+
         }
 
         protected override void Update()
@@ -39,6 +34,28 @@ namespace Blocks
                 var stat = ActorOnBlock.GetAct<CharacterStatAct>();
                 stat?.Damage(int.MaxValue, this);
             }
+        }
+        
+        private void CreateLight()
+        {
+            var lightObj = new GameObject("Light");
+            var light = lightObj.AddComponent<Light>();
+            light.type = LightType.Point;
+            light.range = 1.5f;
+            light.intensity = 5f;
+            light.color = Color.white;
+            light.shadows = LightShadows.Hard;
+            light.renderMode = LightRenderMode.ForceVertex;
+            lightObj.transform.SetParent(anchorTrm);
+            lightObj.transform.localPosition = Vector3.up * -0.2f;
+        }
+
+        private void CreateParticle()
+        {
+            var resource = Define.GetManager<ResourceManager>();
+            var particle = resource.Instantiate("EB_Particle");
+            particle.transform.SetParent(anchorTrm);
+            particle.transform.localPosition = Vector3.up * 0.25f;
         }
     }
 }
