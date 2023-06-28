bed90c66746672ac5f50807fa37a9af507901d9a
csh4430 <snghun6889@gmail.com>
2023-06-29 00:55:50 +0900

    RisingDoor

---
+++ Assets/01.Scripts/Actors/RisingDoor.cs
@@ -0,0 +1,26 @@
+﻿using Actors.Characters.NPC;
+using DG.Tweening;
+using UnityEngine;
+
+namespace Actors
+{
+    public class RisingDoor : TouchActor
+    {
+        [SerializeField] private Transform risingTrm;
+        [SerializeField] private bool canOpen = false;
+
+        protected override void Update()
+        {
+            if(canOpen)
+                base.Update();
+        }
+
+        public void Rise()
+        {
+            risingTrm.DOLocalMoveY(1, 3).OnComplete(() =>
+            {
+                canOpen = true;
+            });
+        }
+    }
+}
